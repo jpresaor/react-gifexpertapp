fd3da5cdb1cccf65ffec541f4736606601a9ad32
Jairo Presa Ornia <jpresa@intermarkit.es>
2020-09-19 13:35:09 +0200

    Added docs

---
+++ docs/precache-manifest.9e32acb6311e056987b32f988abd1f65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5844c902ccc460b77b31a0ab2a5350d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c28fb3ac7f91e1fea0e8",
+    "url": "/static/css/main.5757e0dc.chunk.css"
+  },
+  {
+    "revision": "708801a2a63a7cf27793",
+    "url": "/static/js/2.28173e04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.28173e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c28fb3ac7f91e1fea0e8",
+    "url": "/static/js/main.1e4a1532.chunk.js"
+  },
+  {
+    "revision": "070518975a2f4d1da29a",
+    "url": "/static/js/runtime-main.03852323.js"
+  }
+]);
